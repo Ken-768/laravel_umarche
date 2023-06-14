26afc211a68205f804cb6324aa289b623bcd5eb8
Ken-768 <kcp.is.revolutionary@gmail.com>
2023-06-15 00:00:10 +0900

    クラスベース

--- resources/views/tests/component-test2.blade.php
+++ resources/views/tests/component-test2.blade.php
@@ -1,4 +1,7 @@
 <x-tests.app>
     <x-slot name="header">ヘッダー2</x-slot>
     コンポーネント2
+    <x-test-class-base classBaseMessage="メッセージ" />
+    <div class="mb-4"></div>
+    <x-test-class-base classBaseMessage="メッセージ" defaultMessage="一回" />
 </x-tests.app>
